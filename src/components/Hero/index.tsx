548b384425e70c24f8d3dd0de2efd3ef7349bb69
goliveira-dev <guigooliveraasam@gmail.com>
2025-07-09 22:35:38 -0300

    da inicio a implementação do mobile first

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,14 @@
-import { HeroContent } from './content';
+import { Link } from 'react-router-dom';
 import './style.css'
 
 function HeroSection() {
   return (
     <section className="hero-section">
       <div className="hero-container">
-        <h1>{HeroContent.title}</h1>
-        <p>{HeroContent.description}</p>
+        <h2>Desenvolvedor Web Full-stack</h2>
+        <h1>Transformando ideias em soluções web robustas e modernas.</h1>
+        <p>Minha especialidade é <span className='destak-text'>tirar suas ideias do papel</span> e transformá-las em <span className='destak-text'>algo real na internet</span>, construo sites e aplicações Web completos, do começo ao fim.</p>
+        <Link className='go-projects-btn' to=''>Ver meus projetos<i className="bi bi-arrow-right-short"></i></Link>
       </div>
     </section>
   )
